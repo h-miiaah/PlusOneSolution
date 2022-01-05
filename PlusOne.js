d7a656c72b08102280d6a2d801f5d62bb43ddc09
h-miiaah <haim.miiaah5@gmail.com>
2022-01-04 22:20:18 -0500

    added conditional

--- PlusOne.js
+++ PlusOne.js
@@ -6,5 +6,11 @@
     let carry = 0;
     for (let i = digits.length - 1 ; i >= 0; i--){
         carry = (digits[i] + 1 == 10)? 1 : 0;
+        if (carry == 0){
+            digits[i] = digits[i] + 1;
+            break;
+        }else{
+            digits[i] = 0 ; 
+        }
     }
 };
